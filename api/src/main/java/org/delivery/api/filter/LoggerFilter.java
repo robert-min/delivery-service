0016c1653fa502e018077db963f0ffd89d5410b6
robert-min <robertmin522@gmail.com>
2023-08-19 20:55:49 +0900

    feat : Add LoggerFilter

---
+++ api/src/main/java/org/delivery/api/filter/LoggerFilter.java
@@ -0,0 +1,61 @@
+package org.delivery.api.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class LoggerFilter implements Filter {
+
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        var req = new ContentCachingRequestWrapper((HttpServletRequest) request);
+        var res = new ContentCachingResponseWrapper((HttpServletResponse) response);
+        // 실행 전
+
+        chain.doFilter(req, res);
+
+        // 실행 후
+
+        // Request 정보
+        var headerNames = req.getHeaderNames();
+        var headerValues = new StringBuilder();
+
+        headerNames.asIterator().forEachRemaining(headerKey -> {
+            var headerValue = req.getHeader(headerKey);
+
+            headerValues.append("[").append(headerKey).append(" : ").append(headerValue).append("] ");
+        });
+
+        var requestBody = new String(req.getContentAsByteArray());
+        var uri = req.getRequestURI();
+        var method = req.getMethod();
+
+        log.info(">>>>> uri : {}, method : {}, header : {}, body : {}", uri, method, headerValues, requestBody);
+
+        // response 정보
+        var responseHeaderValues = new StringBuilder();
+
+        res.getHeaderNames().forEach(headerKey -> {
+            var headerValue = res.getHeader(headerKey);
+
+            responseHeaderValues.append("[").append(headerKey).append(" : ").append(headerValue).append("] ");
+        });
+
+        var responseBody = new String(res.getContentAsByteArray());
+
+        log.info(">>>>> uri : {}, method : {}, header : {}, body : {}", uri, method, headerValues, requestBody);
+
+        res.copyBodyToResponse();
+
+    }
+}
