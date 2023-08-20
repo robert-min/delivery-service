09f7bbcf4602a25115b760012fcb4649b8c95eb9
robert-min <robertmin522@gmail.com>
2023-08-20 13:50:59 +0900

    feat : Add user auth validation with using interceptor session

--- api/src/main/java/org/delivery/api/domain/user/business/UserBusiness.java
+++ api/src/main/java/org/delivery/api/domain/user/business/UserBusiness.java
@@ -10,6 +10,7 @@ import org.delivery.api.domain.user.controller.model.UserLoginRequest;
 import org.delivery.api.domain.user.controller.model.UserRegisterRequest;
 import org.delivery.api.domain.user.controller.model.UserResponse;
 import org.delivery.api.domain.user.converter.UserConverter;
+import org.delivery.api.domain.user.model.User;
 import org.delivery.api.domain.user.service.UserService;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -60,11 +61,9 @@ public class UserBusiness {
         return tokenResponse;
     }
 
-    public UserResponse me() {
-        var requestContext = Objects.requireNonNull(RequestContextHolder.getRequestAttributes());
-        var userId = requestContext.getAttribute("userId", RequestAttributes.SCOPE_REQUEST);
+    public UserResponse me(User user) {
 
-        var userEntity = userService.getUserWithThrow(Long.parseLong(userId.toString()));
+        var userEntity = userService.getUserWithThrow(user.getId());
         var response = userConverter.toResponse(userEntity);
         return response;
     }
---
+++ api/src/main/java/org/delivery/api/resolver/UserSessionResolver.java
@@ -0,0 +1,58 @@
+package org.delivery.api.resolver;
+
+import lombok.RequiredArgsConstructor;
+import org.delivery.api.common.annotation.UserSession;
+import org.delivery.api.domain.user.business.UserBusiness;
+import org.delivery.api.domain.user.model.User;
+import org.delivery.api.domain.user.service.UserService;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+@RequiredArgsConstructor
+public class UserSessionResolver implements HandlerMethodArgumentResolver {
+
+    private final UserService userService;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        // 지원하는 파리미터, 어노테이션 체크
+        // 1. annotation check
+        var annotation = parameter.hasParameterAnnotation(UserSession.class);
+        // 2. parameter type check
+        var parameterType = parameter.getParameterType().equals(User.class);
+
+        return (annotation && parameterType);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        // support parameter에서 True 반환 시 실행
+
+        // request holder
+        var requestContext = RequestContextHolder.getRequestAttributes();
+        var userId = requestContext.getAttribute("userId", RequestAttributes.SCOPE_REQUEST);
+
+        var userEntity = userService.getUserWithThrow(Long.parseLong(userId.toString()));
+
+        // 사용자 정보 셋팅
+        return User.builder()
+                .id(userEntity.getId())
+                .name(userEntity.getName())
+                .email(userEntity.getEmail())
+                .status(userEntity.getStatus())
+                .password(userEntity.getPassword())
+                .address(userEntity.getAddress())
+                .registeredAt(userEntity.getRegisteredAt())
+                .unregisteredAt(userEntity.getUnregisteredAt())
+                .lastLoginAt(userEntity.getLastLoginAt())
+                .build();
+
+    }
+}
