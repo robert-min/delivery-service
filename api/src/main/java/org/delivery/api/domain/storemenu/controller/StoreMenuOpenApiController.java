2ecf2b96176e760673810162f27e68a6c5eab380
robert-min <robertmin522@gmail.com>
2023-08-21 13:10:40 +0900

    feat : Add StoreMenu api

---
+++ api/src/main/java/org/delivery/api/domain/storemenu/controller/StoreMenuOpenApiController.java
@@ -0,0 +1,31 @@
+package org.delivery.api.domain.storemenu.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.delivery.api.common.api.Api;
+import org.delivery.api.domain.storemenu.business.StoreMenuBusiness;
+import org.delivery.api.domain.storemenu.controller.model.StoreMenuRegisterRequest;
+import org.delivery.api.domain.storemenu.controller.model.StoreMenuResponse;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/open-api/store-menu")
+public class StoreMenuOpenApiController {
+
+    private final StoreMenuBusiness storeMenuBusiness;
+
+    @PostMapping("/register")
+    public Api<StoreMenuResponse> register(
+            @Valid
+            @RequestBody Api<StoreMenuRegisterRequest> request
+    ) {
+        var req = request.getBody();
+        var resposne = storeMenuBusiness.register(req);
+        return Api.OK(resposne);
+    }
+}
