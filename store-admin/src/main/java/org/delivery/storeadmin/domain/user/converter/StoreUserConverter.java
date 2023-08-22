728a4acea4f34c5e93660fc898d0f49c226e297b
robert-min <robertmin522@gmail.com>
2023-08-22 11:30:29 +0900

    feat : Add spring security authorization

--- store-admin/src/main/java/org/delivery/storeadmin/domain/user/converter/StoreUserConverter.java
+++ store-admin/src/main/java/org/delivery/storeadmin/domain/user/converter/StoreUserConverter.java
@@ -5,6 +5,7 @@ import org.delivery.db.store.StoreEntity;
 import org.delivery.db.store.StoreRepository;
 import org.delivery.db.store.enums.StoreStatus;
 import org.delivery.db.storeuser.StoreUserEntity;
+import org.delivery.storeadmin.domain.authorization.model.UserSession;
 import org.delivery.storeadmin.domain.user.controller.model.StoreUserRegisterRequest;
 import org.delivery.storeadmin.domain.user.controller.model.StoreUserResponse;
 import org.delivery.storeadmin.domain.user.service.StoreUserService;
@@ -49,6 +50,30 @@ public class StoreUserConverter {
                         StoreUserResponse.StoreResponse.builder()
                                 .id(storeEntity.getId())
                                 .name(storeEntity.getName())
+                                .build()
+                )
+                .build();
+    }
+
+    public StoreUserResponse toResponse(
+            UserSession userSession
+    ) {
+        return StoreUserResponse.builder()
+                .user(
+                        StoreUserResponse.UserResponse.builder()
+                                .id(userSession.getUserId())
+                                .email(userSession.getEmail())
+                                .status(userSession.getStatus())
+                                .role(userSession.getRole())
+                                .registeredAt(userSession.getRegisteredAt())
+                                .unregisteredAt(userSession.getUnregisteredAt())
+                                .lastLoginAt(userSession.getLastLoginAt())
+                                .build()
+                )
+                .store(
+                        StoreUserResponse.StoreResponse.builder()
+                                .id(userSession.getStoreId())
+                                .name(userSession.getStoreName())
                                 .build()
                 )
                 .build();
