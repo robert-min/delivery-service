10b855e5bcf9b2ed52ffa776f638ff57ffa19192
robert-min <robertmin522@gmail.com>
2023-08-23 13:12:59 +0900

    feat : Add push sse finish

--- store-admin/src/main/java/org/delivery/storeadmin/domain/userorder/business/UserOrderBusiness.java
+++ store-admin/src/main/java/org/delivery/storeadmin/domain/userorder/business/UserOrderBusiness.java
@@ -3,9 +3,15 @@ package org.delivery.storeadmin.domain.userorder.business;
 import lombok.RequiredArgsConstructor;
 import org.delivery.common.message.model.UserOrderMessage;
 import org.delivery.storeadmin.domain.sse.connection.SseConnectionPool;
+import org.delivery.storeadmin.domain.storemenu.converter.StoreMenuConverter;
+import org.delivery.storeadmin.domain.storemenu.service.StoreMenuService;
+import org.delivery.storeadmin.domain.userorder.controller.model.UserOrderDetailResponse;
 import org.delivery.storeadmin.domain.userorder.converter.UserOrderConverter;
 import org.delivery.storeadmin.domain.userorder.service.UserOrderService;
+import org.delivery.storeadmin.domain.userordermenu.service.UserOrderMenuService;
 import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +20,9 @@ public class UserOrderBusiness {
     private final UserOrderService userOrderService;
     private final SseConnectionPool sseConnectionPool;
     private final UserOrderConverter userOrderConverter;
+    private final UserOrderMenuService userOrderMenuService;
+    private final StoreMenuService storeMenuService;
+    private final StoreMenuConverter storeMenuConverter;
 
     /*
     * 주문
@@ -30,17 +39,33 @@ public class UserOrderBusiness {
         // user order entity
 
         // user order menu
+        var userOrderMenuList = userOrderMenuService.getUserOrderMenuList(userOrderEntity.getId());
 
         // user order menu -> store menu
+        var storeMenuEntityList = userOrderMenuList.stream()
+                .map(userOrderMenuEntity -> {
+                    return storeMenuService.getStoreMenuWithThrow(userOrderMenuEntity.getStoreMenuId());
+                })
+                .map(storeMenuEntity -> {
+                    return storeMenuConverter.toResponse(storeMenuEntity);
+                })
+                .collect(Collectors.toList());
+
+        var userOrderResponse = userOrderConverter.toResponse(userOrderEntity);
 
         // response
+
+        var push = UserOrderDetailResponse.builder()
+                .userOrderResponse(userOrderResponse)
+                .storeMenuResponseList(storeMenuEntityList)
+                .build();
 
         // push
 
         var userConnection = sseConnectionPool.getSession(userOrderEntity.getStoreId().toString());
 
         // 사용자에게 push
-//        userConnection.sendMessage();
+        userConnection.sendMessage(push);
 
 
 
